2f0615452f748dd3e5688baf39dec26106db9825
Rivery.Wang <ywang@qnventures.com>
2026-04-17 20:43:04 +0800

    feat: update

---
+++ src/api/flat-blog-post/controllers/concat-blog-post.js
@@ -0,0 +1,3 @@
+"use strict";
+const { createCoreController } = require("@strapi/strapi").factories;
+module.exports = createCoreController("api::flat-blog-post.flat-blog-post");
---
+++ src/api/flat-blog-post/routes/concat-blog-post.js
@@ -0,0 +1,3 @@
+"use strict";
+const { createCoreRouter } = require("@strapi/strapi").factories;
+module.exports = createCoreRouter("api::flat-blog-post.flat-blog-post");
